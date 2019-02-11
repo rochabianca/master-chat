b287a651cf334ad7a686d44ea711d7fe6d108a40
Bianca <bianca.rocha1711@gmail.com>
2019-02-11 08:55:29 -0300

    using moment to format data

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -21,6 +21,7 @@
 <script>
 import NewMessage from "@/components/NewMessage";
 import db from "@/firebase/init";
+import moment from "moment";
 
 export default {
   name: "Chat",
@@ -44,7 +45,7 @@ export default {
             id: doc.id,
             name: doc.data().name,
             content: doc.data().content,
-            timestamp: doc.data().timestamp
+            timestamp: moment(doc.data().timestamp).format("lll")
           });
         }
       });
@@ -64,7 +65,7 @@ export default {
   }
   .time {
     display: block;
-    font-size: 1.2em;
+    font-size: 0.8em;
   }
 }
 </style>
